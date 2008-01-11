5fbf96d948210442f07328f46816e57be74a4fa9
Nils-Helge Garli Hegvik <nilsga@gmail.com>
2008-01-11 08:41:15 +0000

    Implemented pluto 2.0 support
    git-svn-id: https://boss.bekk.no/repos/projects/maven-jetty-pluto-embedded/branches/2_0_BRANCH@2168 43881228-ca37-0410-962c-81b306aa3a78

---
+++ src/main/java/com/bekk/boss/pluto/embedded/jetty/util/PlutoJettySessionManager.java
@@ -0,0 +1,30 @@
+package com.bekk.boss.pluto.embedded.jetty.util;
+
+import java.lang.reflect.Field;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+
+import org.apache.pluto.internal.impl.PortletSessionImpl;
+import org.mortbay.jetty.servlet.HashSessionManager;
+
+public class PlutoJettySessionManager extends HashSessionManager {
+
+	public Cookie access(HttpSession session, boolean secure) {
+		if (session instanceof PortletSessionImpl) {
+			PortletSessionImpl realSession = (PortletSessionImpl) session;
+			try {
+				Field sessionField = realSession.getClass().getDeclaredField(
+						"httpSession");
+				sessionField.setAccessible(true);
+				HttpSession sess = (HttpSession) sessionField.get(realSession);
+				return super.access(sess, secure);
+			} catch (Exception e) {
+				throw new RuntimeException(e.getMessage(), e);
+			}
+		} else {
+			return super.access(session, secure);
+		}
+	}
+
+}
